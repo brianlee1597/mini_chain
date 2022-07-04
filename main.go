f3e0fa531230e1f0301461c4833dae5a599bfa90
Brian Lee <brianlee1597@gmail.com>
2022-07-03 21:01:28 -0400

    initial setup

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import(
+	"fmt"
+	"bytes"
+	"crypto/sha256"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks) - 1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main () {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First Block After Genesis")
+	chain.AddBlock("Second Block After Genesis")
+	chain.AddBlock("Third Block After Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+}
